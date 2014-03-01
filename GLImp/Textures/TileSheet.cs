4dc00b1c33e6820a135100f09641797cb419f201
Nicholas Rodine <halfofastaple@gmail.com>
2014-02-28 23:52:20 -0500

    Upgraded TileSheet.

--- GLImp/Textures/TileSheet.cs
+++ GLImp/Textures/TileSheet.cs
@@ -8,18 +8,38 @@ using System.Drawing;
 
 namespace GLImp {
 	public class TileSheet {
-		int TileWidth;
-		int TileHeight;
-		Texture texture;
+		public int TileWidth;
+		public int TileHeight;
+		public Texture Texture;
+		public int HOffset;
+		public int VOffset;
+		public int HSeparation;
+		public int VSeparation;
 
 		public TileSheet(Texture tex, int TileWidth, int TileHeight) {
-			texture = tex;
+			this.Texture = tex;
 			this.TileWidth = TileWidth;
 			this.TileHeight = TileHeight;
+			this.HOffset = 0;
+			this.VOffset = 0;
+			this.HSeparation = 0;
+			this.VSeparation = 0;
+		}
+
+		public int HorizontalTiles {
+			get {
+				return (Texture.Width - HOffset) / (TileWidth + HSeparation);
+			}
+		}
+
+		public int VerticalTiles {
+			get {
+				return (Texture.Height - VOffset) / (TileHeight + VSeparation);
+			}
 		}
 
 		public SubImage GetSubImage(int x, int y) {
-			return texture.Subimage(TileWidth * x, TileHeight * y, TileWidth, TileHeight);
+			return Texture.Subimage(((TileWidth + HOffset) * x) + HOffset, ((TileHeight + VOffset) * y) + VOffset, TileWidth, TileHeight);
 		}
 
 		public SubImage this[int x, int y] {
