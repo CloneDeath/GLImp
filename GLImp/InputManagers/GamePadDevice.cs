3053cd871cc0ca210c0551dec823ce95e96769bd
Nicholas Rodine <halfofastaple@gmail.com>
2014-03-09 16:20:00 -0400

    Added missing classes.

---
+++ GLImp/InputManagers/GamePadDevice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Input;
+
+namespace GLImp {
+	public class GamePadDevice {
+		public int DeviceID {
+			get;
+			private set;
+		}
+		public GamePadState State;
+		public GamePadState PreviousState;
+		public GamePadCapabilities Capabilities;
+
+		internal GamePadDevice(int DeviceID) {
+			this.DeviceID = DeviceID;
+		}
+
+		public bool IsConnected {
+			get {
+				return State.IsConnected;
+			}
+		}
+
+		internal void Update() {
+			PreviousState = State;
+			State = OpenTK.Input.GamePad.GetState(DeviceID);
+			Capabilities = OpenTK.Input.GamePad.GetCapabilities(DeviceID);
+		}
+
+		public bool IsDown(OpenTK.Input.Buttons Button)
+		{
+			return State.Buttons.GetButton(Button) == ButtonState.Pressed;
+		}
+
+		public bool IsUp(OpenTK.Input.Buttons Button)
+		{
+			return State.Buttons.GetButton(Button) == ButtonState.Released;
+		}
+
+		public bool IsPressed(OpenTK.Input.Buttons Button)
+		{
+			return State.Buttons.GetButton(Button) == ButtonState.Pressed && PreviousState.Buttons.GetButton(Button) == ButtonState.Released;
+		}
+
+		public bool IsReleased(OpenTK.Input.Buttons Button)
+		{
+			return State.Buttons.GetButton(Button) == ButtonState.Released && PreviousState.Buttons.GetButton(Button) == ButtonState.Pressed;
+		}
+	}
+}
---
+++ GLImp/InputManagers/GamePadManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GLImp {
+	public class GamePadManager {
+		public const int MaxJoysticks = 4;
+		public static GamePadDevice[] GamePads = new GamePadDevice[MaxJoysticks];
+
+		internal static void Init() {
+			for (int i = 0; i < MaxJoysticks; i++) {
+				GamePads[i] = new GamePadDevice(i);
+			}
+		}
+
+		internal static void Update() {
+			foreach (GamePadDevice dev in GamePads) {
+				dev.Update();
+			}
+		}
+	}
+}
